3fffd434a5083ddf7bacbdcf3b4631cddc4ea9d5
Luis Roth <vitlroth@gmail.com>
2017-05-20 00:37:03 -0300

    *.teste

---
+++ src/br/edu/unisinos/main/Help.java
@@ -0,0 +1,34 @@
+package br.edu.unisinos.main;
+
+public class Help {
+	
+	
+	private String a =  " ";
+	private String b = " ";
+	
+	
+	
+	public String getA() {
+		return a;
+	}
+	
+	
+	public String getB() {
+		return b;
+	}
+	
+	
+	
+	public void setA(String a) {
+		this.a = a;
+	}
+	
+	
+	
+	public void setB(String b) {
+		this.b = b;
+	}
+	
+	
+
+}
